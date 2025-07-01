d9a8bd25af7adfaa2fb464d05c4239d498152741
Horie Kyazimova <horish99@gmail.com>
2025-07-01 17:10:27 +0300

    Fix

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   const [theme, setTheme] = useState(() => {
-    return localStorage.getItem("theme") || "dark";
+    return localStorage.getItem("theme") || "light";
   });
 
   useEffect(() => {
